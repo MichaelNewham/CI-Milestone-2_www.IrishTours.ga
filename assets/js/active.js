36322cd9efa439e8b48980ed4f399f901e8049c5
michaelnewham <mail2mick@gmail.com>
2021-05-07 14:17:55 +0000

    Removed typo from active.js file, and added extra function in active.js file

--- assets/js/active.js
+++ assets/js/active.js
@@ -1,7 +1,20 @@
 (function ($) {
     'use strict';
 
-    // Preloader Active Code
+    if ($.fn.init) {
+        new WOW().init();
+    }
+
+    var $window = $(window);
+
+    $window.on('scroll', function () {
+        if ($window.scrollTop() > 0) {
+            $('body').addClass('sticky');
+        } else {
+            $('body').removeClass('sticky');
+        }
+    });
+
     $window.on('load', function () {
         $('#preloader').fadeOut('slow', function () {
             $(this).remove();
